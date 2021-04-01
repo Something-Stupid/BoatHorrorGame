de359c28d89a4a18766f20539a4fc2ecd716657d
LeonWadsworth <indyyt07@gmail.com>
2021-03-31 21:44:18 -0400

    Added Flashlight Script and Added the walk and flashlight test scene.

---
+++ Assets/Scripts/Flashlight/Flashlighttoggle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlighttoggle : MonoBehaviour
+{
+    public Light flashlight;
+
+    public bool on = true;
+    
+
+
+    void Start()
+    {
+        on = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if(on == true)
+            {
+                flashlight.enabled = false;
+                on = false;
+            }
+            else
+            {
+                flashlight.enabled = true;
+                on = true;
+            }
+        }
+    }
+}
